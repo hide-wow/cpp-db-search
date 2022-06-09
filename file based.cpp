980dbfe3e9031a24b7c16a4cc8afc930c676683e
HideLeOuf <78696725+hide-wow@users.noreply.github.com>
2022-06-09 20:20:04 +0200

    yep, just a little update ;)
    yep, just a little update ;)

---
+++ file based.cpp
@@ -0,0 +1,77 @@
+#include <filesystem>
+namespace fs = std::filesystem;
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// too lazy to comment this, just read and learn by yourself.
+std::string getKeyword() {
+    string line;
+    string keyword;
+    ifstream myfile ("keyword.txt");
+
+    if (myfile.is_open())
+    {
+        while ( getline (myfile,line) ) 
+        {
+            keyword = line;
+        }
+    }
+    else
+    {
+        cout << "Unable to open the keyword file";
+        exit(0);
+    }
+    return keyword;
+}
+
+// useless but why not, i'll maybe use it later
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "An error occurred while opening the file : '" << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
+// too lazy to comment this too
+bool is_file_empty(std::ifstream& pFile) {
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+int main() {
+    fs::path path = fs::current_path(); // /!\ don't work on cmd
+    string dbPath = path.u8string() + "\\db\\"; // setup the db path
+    string line; // setup the line var
+    string keyword = getKeyword(); // setup the keyword
+    ofstream outfile;
+    outfile.open("output.txt", ios_base::app);
+
+    // for each file in the db directory
+    for (const auto & entry : fs::directory_iterator(dbPath))
+    {
+        ifstream dbfile (entry.path()); // setup the file to be read
+        if (dbfile.is_open()) // verify it can be opened (if it exist)
+        {
+            while ( getline (dbfile,line) ) // get all lines of the file
+            {
+                if (line.find(keyword) != string::npos) // if you find the keyword
+                {
+                    // write the line to the output file
+                    outfile << line << endl;
+                }
+            }
+
+        dbfile.close();
+        }
+
+        else
+        {
+            cout << "Unable to open the db file"; 
+        }
+    }
+    outfile.close();
+    return 0;
+}
---
+++ text based.cpp
@@ -0,0 +1,90 @@
+#include <filesystem>
+#include <windows.h>
+namespace fs = std::filesystem;
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void wait() {
+    // press any key to continue
+    cout << "Press any key to continue...";
+    cin.ignore();
+    cin.get();
+}
+
+void menu() {
+    cout << "\n                 / /                        / \n _. _   _     __/ /__   _   _  __.  __  _. /_ \n(__/_)_/_)_  (_/_/_)   /_)_</_(_/|_/ (_(__/ /_\n  /   /                                       \n '   '                                        " << endl;
+}
+
+// useless but why not, i'll maybe use it later
+string readFileIntoString(const string& path) {
+    ifstream input_file(path);
+    if (!input_file.is_open()) {
+        cerr << "An error occurred while opening the file : '" << path << "'" << endl;
+        exit(EXIT_FAILURE);
+    }
+    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
+// too lazy to comment this too
+bool is_file_empty(std::ifstream& pFile) {
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+int main() {
+    HWND hWnd = GetConsoleWindow();
+    ShowWindow(hWnd,SW_SHOWMAXIMIZED);  
+    menu();
+    bool something = false;
+    int i = 1;
+    string separation = "------------";
+    fs::path path = fs::current_path(); // /!\ don't work on cmd
+    string dbPath = path.u8string() + "\\db\\"; // setup the db path
+    string line; // setup the line var
+    string keyword; // setup the keyword var
+    cout << "Enter a keyword : ";
+    cin >> keyword;
+    cout << separation << endl;
+
+    // for each file in the db directory
+    for (const auto & entry : fs::directory_iterator(dbPath))
+    {
+        ifstream dbfile (entry.path()); // setup the file to be read
+        if (dbfile.is_open()) // verify it can be opened (if it exist)
+        {
+            while ( getline (dbfile,line) ) // get all lines of the file
+            {
+                if (line.find(keyword) != string::npos) // if you find the keyword
+                {
+                    // write the line to the output file and show in the tui
+                    something = true;
+                    cout << "[" << i << "] - " << line << endl;
+                    cout << separation << endl;
+                    i++;
+                }
+                else
+                {
+                    // if you don't find the keyword, do nothing
+                }
+            }
+
+        dbfile.close();
+        }
+
+        else
+        {
+            cout << "Unable to open the db file"; 
+        }
+    }
+    if (something == false)
+    {
+        cout << "No result found" << endl;
+        wait();
+    }
+    else 
+    {
+        wait();
+    }
+    return 0;
+}
